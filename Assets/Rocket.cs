eb4d7c7ab1a651f0d308a24d7a6d823fff0466fe
Salman Naqvi <salmananaqvii+atlassianbitbucket@gmail.com>
2019-08-17 20:42:23 +0500

    Added the framework the the rocket's movement logic.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.W)) // 'W' has its own if statement so that turning while thrusting can happen
+        {
+            print("'W' pressed");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("'A' pressed");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("'D' pressed");
+        } 
+    }
+}
