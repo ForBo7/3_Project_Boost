9d028dc14ee2989042c957cbf0d72f3da1da42d2
Salman Naqvi <salmananaqvii+atlassianbitbucket@gmail.com>
2019-08-21 21:16:26 +0500

    Added oscillation script.

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor; // 0 for not moved, 1 for fully moved.
+
+    Vector3 startingPosition; // must be stored for absolute movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
